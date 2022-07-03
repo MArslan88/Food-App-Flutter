67c5913a85a63d59d1223a5871f8fb866a35d519
MArslan88 <gery_maz_555@hotmail.com>
2022-07-03 06:27:42 +0500

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:food_app/screens/landing_screen.dart';
 import 'package:food_app/utils/constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Screen 2',
       theme: ThemeData(textTheme: defaultText),
+      home: LandingScreen(),
     );
   }
 }
