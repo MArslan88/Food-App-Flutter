1a9532e8aeb17b5e9bdc66d2560c9c519f81a18d
MArslan88 <gery_maz_555@hotmail.com>
2022-07-03 07:29:01 +0500

    header section-1

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/utils/constants.dart';
+
+import '../utils/widget_functions.dart';
 
 const PRODUCT_DATA = [
   {
@@ -53,8 +56,95 @@ class LandingScreen extends StatefulWidget {
 class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: Container(),
+      resizeToAvoidBottomInset: false,
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Container(
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 4,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Container(
+                        color: COLOR_GREEN,
+                      ),
+                      Image.asset('assets/images/tree_v.png'),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child:
+                                        Image.asset('assets/images/avatar.png'),
+                                  ),
+                                ),
+                                addHorizontalSpace(20),
+                                Expanded(
+                                    child: Text(
+                                  "How hungry are you today?",
+                                  style: textTheme.headline5
+                                      ?.apply(color: Colors.white),
+                                )),
+                              ],
+                            ),
+                            TextField(
+                              style: TextStyle(color: Colors.white),
+                              cursorColor: Colors.white,
+                              decoration: InputDecoration(
+                                  hintText: 'Search food items',
+                                  hintStyle: TextStyle(color: Colors.white),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide: BorderSide.none),
+                                  prefixIcon: Icon(
+                                    Icons.search,
+                                    color: Colors.white,
+                                  ),
+                                  suffixIcon: Container(
+                                    width: 70,
+                                    height: 70,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white24,
+                                        borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(20),
+                                            bottomRight:
+                                                Radius.circular(20.0))),
+                                    child: Icon(
+                                      Icons.menu,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white24),
+                            ),
+                            addVerticalSpace(10) // some extra space for margin
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                width: constraints.maxHeight,
+                color: Colors.grey.shade200,
+                height: 400, //for demo only
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
